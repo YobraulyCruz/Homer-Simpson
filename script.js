6406b1fe4f45483532f6f943f236a6183d3f75f8
YobraulyCruz <43036792+YobraulyCruz@users.noreply.github.com>
2019-03-22 09:06:33 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,32 @@
+$(".post-image1").click(function(){
+    $(".dab").show();
+    $(".post-text1").hide();
+});
+
+$(".post-image2").click(function(){
+    $(".oof").show();
+    $(".post-text2").hide();
+});
+
+$(".post-image3").click(function(){
+    $(".spego").show();
+    $(".post-text3").hide();
+});
+
+$(".post-image1").click(function(){
+    $("#header").css("background-color", "yellow");
+    
+});
+
+$("#profile-picture").click(function(){
+    $("#sidebar").css("background-color", "purple");
+    
+});
+
+$("#profile-picture").click(function(){
+    alert("Its Treyway");
+    
+});
+$(".post-image2").click(function(){
+    alert("Onii-chan stooooop");
+});
